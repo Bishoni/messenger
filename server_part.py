9c59e5f5b2654ce5aae1f499c4e60729c8f9accb
openYourEyes <addim111@yandex.ru>
2024-08-03 12:39:05 +0700

    more logs, fix system sending

--- server_part.py
+++ server_part.py
@@ -1,52 +1,58 @@
 import socket
 import threading
 
-HOST = '192.168.0.99'
+HOST = '192.168.0.30'
 PORT = 12345
 clients = []
 
 
-# TODO: Сделать корректный вывод в консоль сервера о отправке сообщений. И от отключений пользователей
 def handle_client(client, addr):
     clients.append(client)
     try:
         while True:
-            message = client.recv(1024).decode()
-            print(f'{addr} отправил сообщение {message.split(":")[1]}')
+            input_data = client.recv(1024).decode()
+            username = input_data.split(':')[0]
+            message = str(input_data.split(':')[1]).lstrip()
             if message == 'disconnect':
+                print(f'LOG: Клиент \'{username}\' разорвал настоящее соединение')
                 break
             else:
-                broadcast(message, client)
-    except Exception as e:
-        print(e)
+                print(f'Участник \'{username}\' отправил сообщение: \'{message}\' ')
+                broadcast(username, message, client)
+    except IndexError as e:
+        leave_user(str(username), send_console=True)
     finally:
-        # Вот в этой строке
-        print(f"Соединение было принудительно разорвано с {addr}")
+        print(f"LOG: Соединение было разорвано с {addr}")
         clients.remove(client)
         client.close()
 
 
-def broadcast(message, sender):
+def broadcast(username, message, sender=None):
     for client in clients:
         if client != sender:
             try:
-                client.send(message.encode())
+                client.send(f'{username}: {message}'.encode())
             except Exception:
-                print(f'Соединение с {client} потеряно. Сообщение этому клиенту не доставлено')
                 client.close()
                 clients.remove(client)
+
+
+def leave_user(username, send_console):
+    broadcast('SYSTEM', f'Участник \'{username}\' отключился')
+    if send_console:
+        print('SYSTEM:', f'Участник \'{username}\' отключился')
 
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen()
-    print(f'Сервер ожидает подключение по IP: {HOST}; PORT: {PORT}')
+    print(f'LOG: Сервер ожидает подключение по IP: {HOST}; PORT: {PORT}')
 
     while True:
         client, addr = server.accept()
         clients.append(client)
-        print(f"Соединение успешно установлено с {addr}")
+        print(f"LOG: Соединение успешно установлено с {addr}")
         thread = threading.Thread(target=handle_client, args=(client, addr))
         thread.start()
 
